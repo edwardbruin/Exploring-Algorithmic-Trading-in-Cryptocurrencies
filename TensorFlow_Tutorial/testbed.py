c468223b09148b3ea32f02a3c439e8dba0a768fd
edwardnbruin@gmail.com <edwardnbruin@gmail.com>
2021-04-22 18:46:12 +1000

    filtered down which columns are taken in, so only 9 of 12 columns are used. the two columns with timestamps and the 'ignore' column are now not used. the loss value is now returning an actual number!

--- TensorFlow_Tutorial/testbed.py
+++ TensorFlow_Tutorial/testbed.py
@@ -6,9 +6,10 @@ import numpy as np
 import matplotlib as plt
 
 header_names = ["Open_time", "Open", "High", "Low", "Close", "Volume", "Close_time", "Quote_asset_volume", "Number_of_trades", "Taker_buy_base_asset_volume", "Taker_buy_quote_asset_volume", "Ignore"]
+usecols = header_names[1:6] + header_names[7:11]
 training_target = header_names[8]
 
-kline_train = pd.read_csv("klinedata.csv", names = header_names)
+kline_train = pd.read_csv("klinedata.csv", names = header_names, usecols = usecols)
 kline_train.head()
 
 kline_features = kline_train.copy()
@@ -30,4 +31,7 @@ input()
 					
 kline_model.fit(kline_features, kline_labels, epochs=10)
 print(f"model fitted")
+input()
+kline_model.evaluate(kline_features, kline_labels, verbose=2)
+print(f"evaluated")
 input()
