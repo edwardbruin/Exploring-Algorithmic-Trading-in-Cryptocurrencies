4cb41c1f3d6bba59b114521c055e32ae8f4c6885
edwardnbruin@gmail.com <edwardnbruin@gmail.com>
2021-04-23 15:22:24 +1000

    changed the neurons and epochs to be controlled by user inputs

--- TensorFlow_Tutorial/testbed.py
+++ TensorFlow_Tutorial/testbed.py
@@ -5,6 +5,10 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+# layer1_neurons = 64
+# epochs = 20
+layer1_neurons = int(input("layer1_neurons"))
+epochs = int(input("epochs"))
 header_names = ["Open_time", "Open", "High", "Low", "Close", "Volume", "Close_time", "Quote_asset_volume", "Number_of_trades", "Taker_buy_base_asset_volume", "Taker_buy_quote_asset_volume", "Ignore"]
 usecols = header_names[1:6] + header_names[7:11]
 training_target = header_names[8]
@@ -18,7 +22,7 @@ kline_features = np.array(kline_features)
 print(f"data loaded")
 input()
 kline_model = tf.keras.Sequential([
-	layers.Dense(64),
+	layers.Dense(layer1_neurons),
 	layers.Dense(1)
 ])
 print(f"model defined")
@@ -29,7 +33,7 @@ kline_model.compile(loss = tf.losses.MeanSquaredError(),
 print(f"model compiled")
 input()
 					
-kline_model.fit(kline_features, kline_labels, epochs=10)
+kline_model.fit(kline_features, kline_labels, epochs=epochs)
 print(f"model fitted")
 input()
 kline_model.evaluate(kline_features, kline_labels, verbose=2)
